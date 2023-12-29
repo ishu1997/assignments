b8a1a3a894cc379119d835f893051d1f3729e40f
ishu1997 <mightymajestic1997@gmail.com>
2023-12-29 20:33:58 +0530

    week 3: part 2 JWT token completed

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,12 @@
 const jwt = require('jsonwebtoken');
+const zod = require('zod');
 const jwtPassword = 'secret';
+
+const schema = zod.object( {
+    username :zod.string().email("This is not a valid email."),
+    password :zod.string().min(6)
+});
+
 
 
 /**
@@ -14,7 +21,19 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+
+const info = {
+    username:username,
+    password:password
+};
+const response  = schema.safeParse(info)
+   if(!response.success){
+    return null;
+   }
+   
+const token  = jwt.sign({username:username},jwtPassword);
+
+return token;
 }
 
 /**
@@ -26,18 +45,27 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    try{
+   const verified = jwt.verify(token,jwtPassword);
+   return verified ? true:false;
+    } catch(e){
+        return false;
+    }
 }
 
 /**
  * Decodes a JWT to reveal its payload without verifying its authenticity.
  *
  * @param {string} token - The JWT string to decode.
- * @returns {object|false} The decoded payload of the JWT if the token is a valid JWT format.
+ * @returns {true|false} The decoded payload of the JWT if the token is a valid JWT format.
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    const verified = jwt.decode(token,jwtPassword);
+    if(verified){
+        return true;
+    }
+    return false;
 }
 
 
