6f47d7593dac7740c70184322517ae8e4084b74f
ishu1997 <mightymajestic1997@gmail.com>
2024-02-04 11:32:24 +0530

    useeffect added

--- week-4/react-basics/frontend/src/App.jsx
+++ week-4/react-basics/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
@@ -8,11 +8,15 @@ function App() {
 
   const [todos,setTodos]=useState([]);
 
-  fetch("http://localhost:3000/todos")
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos")
   .then(async function(res){
     const json=await res.json();
     setTodos(json.todos);
   })
+  },[])
+
+  
 
   return (
     <>
